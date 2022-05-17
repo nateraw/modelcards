953a32acd35422b5ac707ca83c970bd4a14fb09d
nateraw <nxr9266@g.rit.edu>
2022-05-17 15:36:23 -0400

    :bookmark: Bump version to 0.0.4

--- modelcards/__init__.py
+++ modelcards/__init__.py
@@ -1,4 +1,8 @@
+# flake8: noqa
+# There's no way to ignore "F401 '...' imported but unused" warnings in this
+# module, but to preserve other warnings. So, don't check this module at all.
+
 from .card_data import CardData, EvalResult
 from .cards import ModelCard, RepoCard
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
