c75cadcd3568b9f7cccbf988199e0916d6f8b4c3
Nathan Raw <nxr9266@g.rit.edu>
2022-05-17 15:41:48 -0400

    Bump version to 0.0.4 (#11)
    :bookmark: Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import find_packages, setup
 
+
+def get_version() -> str:
+    rel_path = "modelcards/__init__.py"
+    with open(rel_path, "r") as fp:
+        for line in fp.read().splitlines():
+            if line.startswith("__version__"):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
 with open("requirements.txt", "r") as f:
     requirements = f.read().splitlines()
 
 setup(
     name="modelcards",
-    version="0.0.3",
+    version=get_version(),
     author="Nathan Raw",
     author_email="naterawdata@gmail.com",
     description=(
